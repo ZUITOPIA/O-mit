43705a1b8607295211bbb55ffc72fd24f91685a5
ZUITOPIA <korhwngml123@naver.com>
2023-05-01 16:21:17 +0900

    fix: ui detail

--- frontend/o-mit/src/app/components/FineState.tsx
+++ frontend/o-mit/src/app/components/FineState.tsx
@@ -1,6 +1,6 @@
 export default function FineState() {
     return (
-        <div className="bg-white w-1/4 h-20 flex-col p-3">
+        <div className="bg-white w-1/4 h-18 flex-col p-3">
             <h1 className="text-xs text-gray-400 font-semibold flex justify-end">MONEY</h1>
             <div className="flex justify-end items-center">
                 <h1 className="text-xl text-black font-semibold px-2">100,000,000</h1>
