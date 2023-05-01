d54ce060cde42671445485c812a787e5f26e6dfa
ZUITOPIA <korhwngml123@naver.com>
2023-05-01 16:14:57 +0900

    feat: fine state (money component) tailwindcss 적용

--- frontend/o-mit/src/app/components/FineState.tsx
+++ frontend/o-mit/src/app/components/FineState.tsx
@@ -1,3 +1,11 @@
 export default function FineState() {
-  return;
+    return (
+        <div className="bg-white w-1/4 h-20 flex-col p-3">
+            <h1 className="text-xs text-gray-400 font-semibold flex justify-end">MONEY</h1>
+            <div className="flex justify-end items-center">
+                <h1 className="text-xl text-black font-semibold px-2">100,000,000</h1>
+                <h1 className="text-xm  text-black font-semibold">원</h1>
+            </div>
+        </div>
+    );
 }
