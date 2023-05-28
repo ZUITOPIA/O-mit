808797227698f2d1390f7e2ff66f8572ba42e23b
hyeju1123 <hjjun1123@gmail.com>
2023-05-28 20:13:35 +0900

    apply custom theme color

--- frontend/o-mit/src/app/components/nav/AsideNavBar.tsx
+++ frontend/o-mit/src/app/components/nav/AsideNavBar.tsx
@@ -10,16 +10,14 @@ type Props = {
 export default function AsideNavBar({ categories }: Props) {
   return (
     <section>
-      <div className={"hidden xl:block m-4 p-4 bg-[#e9e9e9] rounded-md"}>
+      <div className={"hidden xl:block m-4 p-4 bg-gray100 rounded-md"}>
         {Object.entries(categories).map(([key, value]) => (
           <ul className="w-full" key={key}>
-            <h3 className="text-base font-semibold text-[#959595] p-2">
-              {key}
-            </h3>
+            <h3 className="text-base font-semibold text-gray400 p-2">{key}</h3>
             {value.length !== 0 &&
               value.map((category, index) => (
                 <li
-                  className="max-w-[13rem] whitespace-nowrap text-ellipsis overflow-hidden font-semibold text-[#959595] mx-2 p-2"
+                  className="max-w-[13rem] whitespace-nowrap text-ellipsis overflow-hidden font-semibold text-gray400 mx-2 p-2"
                   key={index}
                 >
                   {category}
@@ -27,8 +25,8 @@ export default function AsideNavBar({ categories }: Props) {
               ))}
           </ul>
         ))}
-        <hr className="w-full border-0.5 border-[#959595] m-2" />
-        <h3 className="text-base font-semibold text-[#959595] p-2">TAGS</h3>
+        <hr className="w-full border-0.5 border-gray400 m-2" />
+        <h3 className="text-base font-semibold text-gray400 p-2">TAGS</h3>
       </div>
       <AddStudy />
       <FineState />
